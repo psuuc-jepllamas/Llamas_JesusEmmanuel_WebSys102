5498efe74e5cdf7061f3733d09e7a4d433ebca1a
Jesus Emmanuel Llamas <128872587+psuuc-jepllamas@users.noreply.github.com>
2025-02-19 20:51:18 +0800

    Delete Activity 5 directory

--- Activity 5/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MathController; //pagdeklara ng Controllers
-
-//Ruta para maipalabas sa url ang mga ginamit sa MathController.php
-Route::get('{operation1}/{val1}/{val2}', [MathController::class, 'compute']);
-
-?>
