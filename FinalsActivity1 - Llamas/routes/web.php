eb592329acee7766146aa615729a320f313b00fe
Jesus Emmanuel Llamas <128872587+psuuc-jepllamas@users.noreply.github.com>
2025-04-04 23:06:01 +0800

    finals activity 1

--- FinalsActivity1 - Llamas/routes/web.php
+++ FinalsActivity1 - Llamas/routes/web.php
@@ -13,6 +13,4 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/dashboard', function (){
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/dashboard', [LoginController::class, 'dashboard'])->name('dashboard');
